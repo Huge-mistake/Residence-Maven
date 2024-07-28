e00544fcfc6a89eb12a33a136f85a139aed6989e
Max <137968615+Huge-mistake@users.noreply.github.com>
2024-07-28 21:00:23 +0800

    CRAFTER should be included into container flag group

--- src/com/bekvon/bukkit/residence/protection/FlagPermissions.java
+++ src/com/bekvon/bukkit/residence/protection/FlagPermissions.java
@@ -238,9 +238,6 @@ public class FlagPermissions {
             if (one.equals(CMIMaterial.DECORATED_POT))
                 matUseFlagList.put(one.getMaterial(), Flags.container);
 
-            if (one.equals(CMIMaterial.CRAFTER))
-                matUseFlagList.put(one.getMaterial(), Flags.container);
-
             if (one.isButton())
                 matUseFlagList.put(one.getMaterial(), Flags.button);
 
@@ -288,6 +285,7 @@ public class FlagPermissions {
         addMaterialToUseFlag(Material.HOPPER, Flags.container);
         addMaterialToUseFlag(Material.DROPPER, Flags.container);
         addMaterialToUseFlag(Material.FURNACE, Flags.container);
+        addMaterialToUseFlag(Material.CRAFTER, Flags.container);
         addMaterialToUseFlag(CMIMaterial.LEGACY_BURNING_FURNACE.getMaterial(), Flags.container);
 
         if (Version.isCurrentEqualOrHigher(Version.v1_14_R1)) {
